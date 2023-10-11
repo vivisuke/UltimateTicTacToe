ed157a808238193f5e41c5b0e931e2657c744977
vivisuke <ntsuda@master.email.ne.jp>
2023-10-12 07:49:47 +0900

    0018 ◎ C 10/12 10/12 0.100│ToDo│ディセーブル時に各対戦相手ボタン押下時メッセージ表示

--- UltimateTicTacToe/scripts/top_scene.gd
+++ UltimateTicTacToe/scripts/top_scene.gd
@@ -1,24 +1,29 @@
 extends Node2D
 
-
-var oppo_list = []
+const disabled_mess = [
+	"ばぶばぶー", "もっと強くなってから出直して来てね", "俺と対戦するのは100年早いぜ",
+	"余と対戦したいとは無礼者め", "もっと修行して出なおしてきてね", "余の辞書にはまだ「対戦」という文字は無い",
+	"もっと実績をつんできてね", "神は対戦は早過ぎると言っている", "まだまだだな",
+]
+var pressed_oppix = -1
+var opp_list = []
 
 func _ready():
-	build_oppo_lst()
+	build_opp_lst()
 	pass # Replace with function body.
-func build_oppo_lst():
-	oppo_list.push_back($HBC1/TextureButton11)
-	oppo_list.push_back($HBC1/TextureButton12)
-	oppo_list.push_back($HBC1/TextureButton13)
-	oppo_list.push_back($HBC2/TextureButton21)
-	oppo_list.push_back($HBC2/TextureButton22)
-	oppo_list.push_back($HBC2/TextureButton23)
-	oppo_list.push_back($HBC3/TextureButton31)
-	oppo_list.push_back($HBC3/TextureButton32)
-	oppo_list.push_back($HBC3/TextureButton33)
-func pos_to_oppoix(pos):
-	for ix in range(oppo_list.size()):
-		var rct = oppo_list[ix].get_global_rect()
+func build_opp_lst():
+	opp_list.push_back($HBC1/TextureButton11)
+	opp_list.push_back($HBC1/TextureButton12)
+	opp_list.push_back($HBC1/TextureButton13)
+	opp_list.push_back($HBC2/TextureButton21)
+	opp_list.push_back($HBC2/TextureButton22)
+	opp_list.push_back($HBC2/TextureButton23)
+	opp_list.push_back($HBC3/TextureButton31)
+	opp_list.push_back($HBC3/TextureButton32)
+	opp_list.push_back($HBC3/TextureButton33)
+func pos_to_oppix(pos):
+	for ix in range(opp_list.size()):
+		var rct = opp_list[ix].get_global_rect()
 		if rct.has_point(pos):
 			return ix
 	return -1
@@ -29,10 +34,22 @@ func _input(event):
 	if event is InputEventMouseButton:
 		print("InputEventMouseButton")
 		var mp : Vector2 = get_global_mouse_position()
-		var ix = pos_to_oppoix(mp)
+		var ix = pos_to_oppix(mp)
 		print("ix = ", ix)
+		if event.is_pressed():
+			pressed_oppix = ix
+		else:
+			if ix == pressed_oppix:
+				show_disabled_mess(ix)
+			pressed_oppix = -1
 
-
+func show_disabled_mess(ix):
+	$AcceptDialog.dialog_text = disabled_mess[ix]
+	$AcceptDialog.connect("confirmed", on_confirmed)
+	$AcceptDialog.show()
+	var sz = $AcceptDialog.size
+	$AcceptDialog.position = Vector2(250 - sz.x/2, 400)
+	pass
 func _on_texture_button_11_pressed():
 	#$ConfirmationDialog.title = ""
 	$ConfirmationDialog.dialog_text = "ばぶばぶー"
